ca3d39c50f5b55748956632ae0a5fb7c003584d2
hairajendra <hairajendra@gmail.com>
2018-01-22 16:07:50 +0530

    service Model renamed to ServiceRole and the dependent changes

---
+++ reference_docs/sample-data/devotee.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM icc.devotee
+LIMIT 0, 1000
+
+-- Date: 2018-01-22 15:59
+*/
+INSERT INTO `devotee` (`id`,`legal-name`,`circle-id`,`spiritual-name`,`gender`,`shiksha-level`,`credit-limit`,`realm`,`username`,`password`,`email`,`emailVerified`,`verificationToken`,`created-on`,`updated-on`,`created-by`,`updated-by`,`access-id`,`gothra`,`nakshatra`,`government-unique-id`,`income-tax-id`,`lpm-id`,`kc-association-date`,`mother-tongue-language-id`) VALUES ('3db35f39-8f62-4178-8993-fca4959536d7','Rajendra K','f36e9c73-d8f4-4bfd-901d-2d2ac7b8a5e5','Rasaraj Shyamsundar Dasa','1','d2d9ce87-8f1c-4848-a816-443fd92dbc8c',3000.00,'','hairajendra@gmail.com','$2a$10$zFrX6zpTaxdZfn/HfQps3.951gSH83GczrbKJinm7IVDglKwaWvL.','hairajendra@gmail.com',0,'','2018-01-01 15:12:38','2018-01-16 09:51:51','string','string','string','string','string','string','string','string','2018-01-01','e35a1dd6-b497-4c9a-aadf-ccb97af97711');
+INSERT INTO `devotee` (`id`,`legal-name`,`circle-id`,`spiritual-name`,`gender`,`shiksha-level`,`credit-limit`,`realm`,`username`,`password`,`email`,`emailVerified`,`verificationToken`,`created-on`,`updated-on`,`created-by`,`updated-by`,`access-id`,`gothra`,`nakshatra`,`government-unique-id`,`income-tax-id`,`lpm-id`,`kc-association-date`,`mother-tongue-language-id`) VALUES ('66424f76-ce14-4cf0-94da-7c23076695f0',NULL,NULL,NULL,NULL,NULL,3000.00,NULL,'it@iskconculturalcentre.com','$2a$10$oj6xqPh5/F05EWob9sqD4.0TtJnozMEQOqFP1loKxvPWIYYpmzmP.',NULL,NULL,NULL,'2018-01-20 04:56:57','2018-01-20 04:56:57',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'e35a1dd6-b497-4c9a-aadf-ccb97af97711');
