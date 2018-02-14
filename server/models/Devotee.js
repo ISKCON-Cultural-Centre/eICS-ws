4a22af07f3f0146cd5eca8ae0001d2d001cba14c
hairajendra <hairajendra@gmail.com>
2018-02-14 09:09:06 +0530

    APIs created to get authorizedRoles & Departments

--- server/models/Devotee.js
+++ server/models/Devotee.js
@@ -1,18 +1,25 @@
 var _ = require('underscore');
+var utils = require('./utils');
+
 	module.exports = function(Devotee) {
 
 
 		Devotee.getRoles = function (options, cb) {
-			if (cb === undefined && typeof options === 'function') {
-				cb = options;
-				options = undefined;
-			  }
+
+			cb = cb || utils.createPromiseCallback();
 
 			Devotee.getApp(function (err, app) {
-		  if (err) throw err;
+			if (err) {
+				fn(err);
+				return fn.promise;
+			}			
 		  var RoleMapping = app.models.ServiceRoleMapping;
-		  var Role = app.models.ServiceRole;
+			var Role = app.models.ServiceRole;
 		  RoleMapping.find({ where : { principalId: options.userId }}, function (err, roleMappings) {
+				if (err) {
+					fn(err);
+					return fn.promise;
+				}		
 			var roleIds = _.uniq(roleMappings
 			  .map(function (roleMapping) {
 				return roleMapping.roleId;
@@ -20,18 +27,81 @@ var _ = require('underscore');
 			var conditions = roleIds.map(function (roleId) {
 			  return { id: roleId };
 			});
-			Role.find({ where: { or: conditions}}, function (err, roles) {
-			  if (err) throw err;
-			  var roleNames = roles.map(function(role) {
-				return role.name;
+ 			Role.find({ where: { or: conditions}}, function (err, roles) {
+				if (err) {
+					fn(err);
+					return fn.promise;
+				}	
+			  var authorizedRoles = roles.map(function(role) {
+				return { roleId: role.id, roleName: role.name};
 			  });
-			  cb(null, {"roles": roleNames});
+				cb(null, {roles});
 			});
 		  });
 		});
-	  };
+		return cb.promise;
+		};
+		
+
+		Devotee.getDepartments = function (options, cb) {
+
+			cb = cb || utils.createPromiseCallback();
+
+			Devotee.getApp(function (err, app) {
+			if (err) {
+				fn(err);
+				return fn.promise;
+			}			
+		  var DepartmentRole = app.models.DepartmentRole;
+			var Department = app.models.Department;
+			Devotee.getRoles(options, function(err, authorizedRoles){
+				if (err) {
+					fn(err);
+					return fn.promise;
+				}				
+				var conditions = authorizedRoles.roles.map(function (roleId) {
+					return { roleId: roleId.id };
+				});
+
+		  DepartmentRole.find({ where : { or: conditions }}, function (err, departmentRoles) {
+				if (err) {
+					fn(err);
+					return fn.promise;
+				}		
+			
+			var departmentIds = _.uniq(departmentRoles
+			  .map(function (departmentRole) {
+				return departmentRole.departmentId;
+			  }));
+			var conditions = departmentIds.map(function (departmentId) {
+			  return { id: departmentId };
+			});
+
+			Department.find({ where: { or: conditions}}, function (err, departments) {
+				if (err) {
+					fn(err);
+					return fn.promise;
+				}	
+			  var authorizedDepartments = departments.map(function(department) {
+				return { departmentId: department.id, departmentName: department.name};
+			  });
+				cb(null, {departments});
+			});
+			});
+		});
+		});
+		return cb.promise;
+		};
+		
+
+
+	  Devotee.remoteMethod('getDepartments', {
+			http: { path: '/getDepartments', verb: 'get' },
+			returns: { arg: 'departments', type: 'Object', root: true}
+			});		
+
 	  Devotee.remoteMethod('getRoles', {
 		http: { path: '/getRoles', verb: 'get' },
-		returns: { arg: 'payload', type: 'Object' }
+		returns: { arg: 'roles', type: 'String', root: true}
 	  });
 	}
