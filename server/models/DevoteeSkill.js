cd27b329606ad37b8388714c00c70ac08ba5352c
hairajendra <hairajendra@gmail.com>
2018-03-12 18:34:29 +0530

    devotee-skill wip

--- server/models/DevoteeSkill.js
+++ server/models/DevoteeSkill.js
@@ -1,3 +1,9 @@
 module.exports = function(DevoteeSkill) {
-	
+    DevoteeSkill.remoteMethod('destroyAll', {
+        isStatic: true,
+        description: 'Delete all matching records',
+        accessType: 'WRITE',
+        accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+        http: {verb: 'del', path: '/'}
+      });	
 };
