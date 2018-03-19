88ed1d2c8136a8a951c0ba69632e2e82c4ac9f8b
hairajendra <hairajendra@gmail.com>
2018-03-19 19:30:55 +0530

    destroy all for devotee-language

--- server/models/DevoteeServiceAvailability.js
+++ server/models/DevoteeServiceAvailability.js
@@ -1,3 +1,9 @@
 module.exports = function(DevoteeServiceAvailability) {
-	
+    DevoteeServiceAvailability.remoteMethod('destroyAll', {
+        isStatic: true,
+        description: 'Delete all matching records',
+        accessType: 'WRITE',
+        accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+        http: {verb: 'del', path: '/'}
+      });	
 };
