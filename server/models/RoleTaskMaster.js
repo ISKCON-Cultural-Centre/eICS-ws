9ae1f6ffcf34bb11399d571a0952f1223e44c572
hairajendra <hairajendra@gmail.com>
2018-04-26 09:24:15 +0530

    destroy all roletaskmaster

--- server/models/RoleTaskMaster.js
+++ server/models/RoleTaskMaster.js
@@ -1,3 +1,9 @@
 module.exports = function(RoleTaskMaster) {
-	
+    ServiceRoleMapping.remoteMethod('destroyAll', {
+        isStatic: true,
+        description: 'Delete all matching records',
+        accessType: 'WRITE',
+        accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+        http: {verb: 'del', path: '/'}
+      });	
 };
