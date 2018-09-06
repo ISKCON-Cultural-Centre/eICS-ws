748104ed9efc86b2b4f8f6331efd5635a06350d5
hairajendra <hairajendra@gmail.com>
2018-09-06 15:38:16 +0530

    added count feature

--- server/models/Devotee.js
+++ server/models/Devotee.js
@@ -60,30 +60,31 @@ module.exports = function(Devotee) {
 /* 						console.log("2a");						
 						console.log(finalWhereFilter); */
 					}
-					finalWhereFilter = finalWhereFilter + '] }';					
+					finalWhereFilter = finalWhereFilter + '] }';		
+					console.log(finalWhereFilter); 
+					otherFilter.where = JSON.parse(finalWhereFilter);			
+					Devotee.find(otherFilter, function (err, devotees) {
+						if (err) {
+							cb(err);
+							return cb.promise;
+						}		
+						
+						if (!devotees.length) { return cb(null, { "devotees": [] });}
+						else 
+						{
+							Devotee.count(otherFilter.where, function (err, devoteeCount)	{
+								// console.log(devotees);
+								console.log(devoteeCount);
+								cb(null, {count: devoteeCount, devoteesList: devotees});
+							});				
+						} 
+					});								
 				});
 			}				
 		}
 
 
-console.log(finalWhereFilter); 
-		otherFilter.where = JSON.parse(finalWhereFilter);			
-		Devotee.find(otherFilter, function (err, devotees) {
-			if (err) {
-				cb(err);
-				return cb.promise;
-			}		
-			
-			if (!devotees.length) { return cb(null, { "devotees": [] });}
-			else 
-			{
-				Devotee.count(otherFilter.where, function (err, devoteeCount)	{
-					// console.log(devotees);
-					console.log(devoteeCount);
-					cb(null, {count: devoteeCount, devoteesList: devotees});
-				});				
-			} 
-		});
+
 	});
 	return cb.promise;
 	};
